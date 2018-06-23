0370f2c533a44d959568a317569a73e8e2718fef
Worldoorboll <derevnin@live.ru>
2018-06-23 19:38:13 +0400

    create addition and subtraction methods

---
+++ src/Calculator.java
@@ -0,0 +1,11 @@
+public class Calculator {
+    public int addition(int a, int b){
+        int c = a + b;
+        return c;
+    }
+
+    public int subtraction(int a, int b){
+        int c = a - b;
+        return c;
+    }
+}
