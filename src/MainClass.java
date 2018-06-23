7f6091938fbe2273ec7f921760d9715c0eb3267a
Worldoorboll <derevnin@live.ru>
2018-06-23 19:41:43 +0400

    output calculator result

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,6 +1,8 @@
 public class MainClass {
     public static void main(String[] args) {
-        System.out.println("Hi");
+        Calculator calc = new Calculator();
+        System.out.println(calc.addition(5, 7));
+        System.out.println(calc.subtraction(86, 15));
 
     }
 }
